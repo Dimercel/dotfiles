4055623af05ad502aa2de4cee3c0d449460ceee5
Dimercel <xolcman@gmail.com>
2016-10-20 14:17:04 +0300

    Update nrepl version

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[venantius/ultra "0.4.1"]
                   [lein-kibit "0.1.2"]
                   [lein-ancient "0.6.10"]
-                  [cider/cider-nrepl "0.12.0"]]}}
+                  [cider/cider-nrepl "0.14.0"]]}}
 
