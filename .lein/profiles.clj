35029fc389905cc51e808064626d9ef6f5680a66
Dimercel <xolcman@gmail.com>
2017-03-30 19:38:31 +0300

    Plugin for Leiningein

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[venantius/ultra "0.4.1"]
                   [lein-kibit "0.1.2"]
                   [lein-ancient "0.6.10"]
+                  [lein-environ "1.1.0"]
                   [cider/cider-nrepl "0.14.0"]]}}
-
