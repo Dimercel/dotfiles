81218e746cc005d1cc6bc0fe6a6bcb41a953b52d
Dimercel <xolcman@gmail.com>
2022-06-03 20:43:28 +0300

    Zenburn theme for awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -42,7 +42,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(awful.util.get_themes_dir() .. "xolcman/theme.lua")
+beautiful.init(awful.util.get_themes_dir() .. "zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
